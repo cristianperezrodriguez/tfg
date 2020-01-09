1e711b277f076125dbe1256a4235f64922166468
Cristian Perez Rodriguez <cristian.perez.rodriguez@gmail.com>
2020-01-09 02:33:19 +0100

    Servei de les preguntes.

---
+++ backendRoles/node-role-based-authorization-api-master/preguntes/preguntes.controller.js
@@ -0,0 +1,47 @@
+﻿const express = require('express');
+const router = express.Router();
+const preguntesService = require('./preguntes.service');
+const authorize = require('_helpers/authorize')
+const Role = require('_helpers/role');
+const multipart = require('connect-multiparty');
+const multipartMiddleware = multipart({
+    uploadDir: './uploads'
+});
+
+router.get('/', authorize(Role.Admin), getAll); // admin only
+router.get('/:id', authorize(), getById);   
+router.post('/update', authorize(), update); 
+router.get('/byAssignatura/:idAssignatura', authorize(), getAllByIdAssignatura);     // all authenticated users
+
+
+module.exports = router;
+
+
+
+function getAll(req, res, next) {
+    preguntesService.getAll()
+        .then(preguntes => res.json(preguntes))
+        .catch(err => next(err));
+}
+
+function getById(req, res, next) {
+    preguntesService.getById(req.params.id)
+        .then(assignatura => assignatura ? res.json(assignatura) : res.sendStatus(404))
+        .catch(err => next(err));
+}
+
+function getAllByIdAssignatura(req, res, next) {
+    preguntesService.getAllByIdAssignatura(req.params.idAssignatura)
+        .then(preguntes => res.json(preguntes))
+        .catch(err => next(err));
+}
+
+function update(req, res, next) {
+    preguntesService.update(req.body)
+        .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
+        .catch(err => next(err));
+}
+
+
+
+
---
+++ backendRoles/node-role-based-authorization-api-master/preguntes/preguntes.service.js
@@ -0,0 +1,52 @@
+﻿const config = require('config.json');
+const jwt = require('jsonwebtoken');
+const Role = require('_helpers/role');
+
+var preguntes = [
+    { id: 1, id_assignatura: 1, id_activitat: 1, numero_pregunta: 1, valoracio_minima: 0, valoracio_maxima: 1, percentatge_correccio: 0.8},
+    { id: 2, id_assignatura: 1, id_activitat: 1, numero_pregunta: 2, valoracio_minima: 0, valoracio_maxima: 1.5, percentatge_correccio: 0.4},
+    { id: 3, id_assignatura: 1, id_activitat: 1, numero_pregunta: 3, valoracio_minima: 0, valoracio_maxima: 3, percentatge_correccio: 0.2},
+    { id: 4, id_assignatura: 1, id_activitat: 1, numero_pregunta: 4, valoracio_minima: 0, valoracio_maxima: 1.5, percentatge_correccio: 0.2},
+    { id: 5, id_assignatura: 1, id_activitat: 1, numero_pregunta: 5, valoracio_minima: 0, valoracio_maxima: 3, percentatge_correccio: 0.1},
+    { id: 6, id_assignatura: 2, id_activitat: 1, numero_pregunta: 1, valoracio_minima: 0, valoracio_maxima: 1.5, percentatge_correccio: 0},
+    { id: 7, id_assignatura: 2, id_activitat: 1, numero_pregunta: 2, valoracio_minima: 0, valoracio_maxima: 3, percentatge_correccio: 0.4},
+    { id: 8, id_assignatura: 2, id_activitat: 1, numero_pregunta: 3, valoracio_minima: 0, valoracio_maxima: 1.5, percentatge_correccio: 0.7},
+    { id: 9, id_assignatura: 2, id_activitat: 1, numero_pregunta: 4, valoracio_minima: 0, valoracio_maxima: 3, percentatge_correccio: 0.7}
+];
+/*
+id: number;
+    codi: string;
+    nom: string;
+    any:string;*/
+
+module.exports = {
+    getAll,
+    getById,
+    getAllByIdActivitat,
+    update
+};
+
+async function getAll() {
+    return preguntes.map(u => {
+        return u;
+    });
+}
+
+async function getById(id) {
+    const activitat = preguntes.find(u => u.id === parseInt(id));
+    if (!activitat) return;
+    return activitat;
+}
+
+async function getAllByIdActivitat(idActivitat) {
+    const activ = preguntes.filter(u =>u.id_activitat === parseInt(idActivitat) );
+    if (!activ) return;
+    return activ;
+}
+
+async function update(body) {
+    var pregunta = getById(body.id);
+    if (!pregunta) return;
+    preguntes = body;
+    return activitat;
+}
