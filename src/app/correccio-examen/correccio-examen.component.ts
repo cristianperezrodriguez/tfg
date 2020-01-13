bb01c57072353319d7d274f060636386c7dd2122
Cristian Perez Rodriguez <cristian.perez.rodriguez@gmail.com>
2020-01-13 02:16:41 +0100

    Afegit pagina de correcció d'examen emulada amb "BD" d'array amb funcionalitats.

--- src/app/correccio-examen/correccio-examen.component.ts
+++ src/app/correccio-examen/correccio-examen.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, AfterContentInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { _ } from 'underscore';
 declare var $: any;
@@ -9,21 +9,97 @@ declare var $: any;
   styleUrls: ['./correccio-examen.component.css']
 })
 export class CorreccioExamenComponent implements OnInit {
-  
+
   examen: any;
-  preguntes: any;
   criteris: any;
   tamanyExamen1: number[] = [2480,10547];
-  tamanyExamen2: number[] = [2480,5976];
-  tamanysCapceleraIPreguntes: number[] = [509,1572 - 509,2162 - 1572,3453 - 2162,3879 - 3453,5042 - 3872,5976 - 5042];
+  tamanyExamen: number[] = [2480,5976];
+  tamanysCapcelera = '509px';
   currentSection = 'pregunta1';
+  currentSectionRendered: string;
+  scrollBarWidth: number;
+  
+  preguntes = [
+    {numero_pregunta: 1, valoracio_minima: 0, valoracio_maxima: 1, puntuacioPersonalitzada: false, tamanyPregunta: 1063, criteris: [
+      {valor: 0.1, descripcio: "Criteri 1", checked: false},
+      {valor: 0.2, descripcio: "Criteri 2", checked: false},
+      {valor: 0.3, descripcio: "Criteri 3", checked: false} 
+      ]},
+      {numero_pregunta: 2, valoracio_minima: 0, valoracio_maxima: 1, puntuacioPersonalitzada: false, tamanyPregunta: 590, criteris: [
+        {valor: 1, descripcio: "Criteri 1", checked: false} 
+      ]},
+      {numero_pregunta: 3, valoracio_minima: 0, valoracio_maxima: 2, puntuacioPersonalitzada: false, tamanyPregunta: 1291, criteris: [
+        {valor: 0.1, descripcio: "Criteri 1", checked: false},
+        {valor: 0.2, descripcio: "Criteri 2", checked: false},
+        {valor: 0.3, descripcio: "Criteri 3", checked: false},
+        {valor: 0.4, descripcio: "Criteri 4", checked: false},
+        {valor: 0.5, descripcio: "Criteri 5", checked: false},
+        {valor: 0.6, descripcio: "Criteri 6", checked: false},
+        {valor: 0.7, descripcio: "Criteri 7", checked: false}  
+      ]},
+      {numero_pregunta: 4, valoracio_minima: 0, valoracio_maxima: 1.5, puntuacioPersonalitzada: false, tamanyPregunta: 426, criteris: [
+        {valor: 1, descripcio: "Criteri 1", checked: false},
+        {valor: 0.5, descripcio: "Criteri 2", checked: false} 
+      ]},
+      {numero_pregunta: 5, valoracio_minima: 0, valoracio_maxima: 3, puntuacioPersonalitzada: false, tamanyPregunta: 1170, criteris: [
+        {valor: 1, descripcio: "Criteri 1", checked: false},
+        {valor: 0.2, descripcio: "Criteri 2", checked: false},
+        {valor: 0.3, descripcio: "Criteri 3", checked: false},
+        {valor: 0.3, descripcio: "Criteri 4", checked: false},
+        {valor: 0.1, descripcio: "Criteri 5", checked: false} 
+      ]},
+      {numero_pregunta: 6, valoracio_minima: 0, valoracio_maxima: 1.5, puntuacioPersonalitzada: false, tamanyPregunta: 934, criteris: [
+        {valor: 1, descripcio: "Criteri 1", checked: false},
+        {valor: 0.5, descripcio: "Criteri 2", checked: false},
+        {valor: 0.3, descripcio: "Criteri 3", checked: false} 
+      ]}
+
+  ];
+  getTamanyCapcalera(){
+    var tamanyCapcelera = 0;
+    var ampladaOAlcada = 1;
+    var imatgeHeight = $("#examen").height();
+    //com que encara no esta carregada la imatge i no sap la
+    //alcada de tal, agada el ample del div que sera el mateix
+    //per a fer la regla de tres
+    if(imatgeHeight == 0){
+      ampladaOAlcada = 0;
+      imatgeHeight = $("#parentDiv").width();
+    }
+    var reglaDeTres = imatgeHeight / this.tamanyExamen[ampladaOAlcada];
+    _.each(this.preguntes,function(pregunta){
+      tamanyCapcelera += pregunta.tamanyPregunta;
+    });
+    tamanyCapcelera = (this.tamanyExamen[1] - tamanyCapcelera) * reglaDeTres;
+    return tamanyCapcelera + 'px';
+  }
+  getTamanyPregunta(numPregunta){
+    var ampladaOAlcada = 1;
+    var imatgeHeight = $("#examen").height();
+    //com que encara no esta carregada la imatge i no sap la
+    //alcada de tal, agada el ample del div que sera el mateix
+    //per a fer la regla de tres
+    if(imatgeHeight == 0){
+      ampladaOAlcada = 0;
+      imatgeHeight = $("#parentDiv").width();
+    }
+    var reglaDeTres = imatgeHeight / this.tamanyExamen[ampladaOAlcada];
+    var tamanyPregunta = (this.preguntes[numPregunta].tamanyPregunta * reglaDeTres);
+    return tamanyPregunta + 'px';
+  }
 
   constructor( public router: Router, public route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.calcularAmpladaDelScrollbar();
     this.onResize();
-    $("#parentDiv").height($("#parentDiv").height() - $(".navegacioPreguntes").height());
-    this.scrollTo("capcaleraExamen");
+    
+    setTimeout(function(){
+      $("#menuCriteris").css({ 'top': $("#toolbar").outerHeight() + 'px)', 'height': 'calc(100% - ' + $("#toolbar").outerHeight() + 'px)' });
+    },1);
+    setTimeout(()=>{    //<<<---    using ()=> syntax
+      this.scrollTo(-1);
+     }, 500);
     _.each($(".menuPregunta"), function(a,b,c){
       if(a.id != "criteris-" + this.currentSection){
         $(a).removeClass("criterisVisibles");
@@ -44,6 +120,9 @@ export class CorreccioExamenComponent implements OnInit {
 
   onSectionChange(sectionId: string) {
     this.currentSection = sectionId;
+    var sectionSplit = this.currentSection.match(/[a-z]+|[^a-z]+/gi);
+    this.currentSectionRendered = sectionSplit[0].charAt(0).toUpperCase() + sectionSplit[0].slice(1) + ' ' + sectionSplit[1];
+
     _.each($(".menuPregunta"), function(a,b,c){
       if(a.id != "criteris-" + this.currentSection){
         $(a).removeClass("criterisVisibles");
@@ -62,19 +141,34 @@ export class CorreccioExamenComponent implements OnInit {
     },this);
   }
 
-  scrollTo(section) {
+  scrollTo(numPregunta) {
     var pixelsFinsAlDiv = 0;
-    _.find($('#parentDiv > div'),function(div){
-      pixelsFinsAlDiv += parseInt($(div).css("marginTop").replace('px', ''));
-      if(div.id != section){
-        pixelsFinsAlDiv += $(div).height();
-      } else {
-        return true;
-      }
-    },this);
-    var parentId = $('#'+section).parent()[0].id;
+    if(numPregunta == -1){
+      pixelsFinsAlDiv = parseInt($('#capcaleraExamen').css("marginTop").replace('px', ''));
+    } else {
+      _.find($('#parentDiv > div'),function(div){
+        pixelsFinsAlDiv += parseInt($(div).css("marginTop").replace('px', ''));
+        if(div.id != ('pregunta' + numPregunta)){
+          pixelsFinsAlDiv += $(div).height();
+        } else {
+          return true;
+        }
+      },this);
+    }
+    
+    var parentId:string;
+    if(numPregunta == -1){
+      parentId = $('#capcaleraExamen').parent()[0].id;
+    } else {
+      parentId = $('#pregunta' + numPregunta).parent()[0].id;
+    }
     var parent = document.getElementById(parentId);
-    var elementMidPositonOffset = pixelsFinsAlDiv - $(parent).height()/2 + $('#'+section).height()/2;
+    var elementMidPositonOffset: number;
+    if(numPregunta == -1){
+      elementMidPositonOffset = pixelsFinsAlDiv - $(parent).height()/2 + $('#capcaleraExamen').height()/2;
+    } else {
+      elementMidPositonOffset = pixelsFinsAlDiv - $(parent).height()/2 + $('#pregunta' + numPregunta).height()/2;
+    }
     parent.scrollTo({
       top: elementMidPositonOffset,
       left: 0,
@@ -90,20 +184,57 @@ export class CorreccioExamenComponent implements OnInit {
     return "assets/examen2.jpg";
   }
   onResize(){
-    var ampladaOAlcada = 1;
-    var imatgeHeight = $("#examen").height();
-    //com que encara no esta carregada la imatge i no sap la
-    //alcada de tal, agada el ample del div que sera el mateix
-    //per a fer la regla de tres
-    if(imatgeHeight == 0){
-      ampladaOAlcada = 0;
-      imatgeHeight = $("#parentDiv").width();
-    }
-    var reglaDeTres = imatgeHeight / this.tamanyExamen2[ampladaOAlcada];
-    _.each($("#parentDiv > div"),function(div,a,b){
-      var tamanyActual = this.tamanysCapceleraIPreguntes[a] * reglaDeTres;
-      $(div).height(tamanyActual);
+    //--------------CONTENIDOR DE LA IMATGE DEL EXAMEN-------------
+    //amagar el scrollbar
+    $("#parentDiv").css('padding-right',this.scrollBarWidth);
+    //sense la barra de sota
+    // $("#parentDiv").css({ 'height': 'calc(100% - ' + $("#navegacioPreguntes").outerHeight() + 'px)' });
+    $("#parentDiv").css({ 'height': '100%' });
+    //sense la barra de criteris
+    $("#parentDiv").css({ 'width': 'calc(100% - ' + $("#menuCriteris").outerWidth() + 'px)' });
+    //barra de navegacio
+    $("#navegacioPreguntes").css({ 'width': 'calc(100% - ' + $("#menuCriteris").outerWidth() + 'px)' });
+    
+    //--------------CONTENIDOR DELS CRITERIS-------------
+    // $("#menuCriteris").css({ 'top': $("#toolbar").outerHeight() + 'px)', 'height': 'calc(100vh - ' + ($("#navegacioPreguntes").outerHeight() + $("#toolbar").outerHeight()) + 'px)' });
+    $("#menuCriteris").css({ 'top': $("#toolbar").outerHeight() + 'px)', 'height': 'calc(100% - ' + $("#toolbar").outerHeight() + 'px)' });
+
+
+    //--------------DIVS OCULTS DE LES PREGUNTES-------------
+    $("#capceleraExamen").height(this.getTamanyCapcalera());
+    _.each($(".divOcultPreguntaExamen"),function(div,a,b){
+      $(div).height(this.getTamanyPregunta(a));
     },this);
   }
+
+  calcularAmpladaDelScrollbar(){
+    // Create the measurement node
+    var scrollDiv = document.createElement("div");
+    scrollDiv.className = "scrollbar-measure";
+    document.body.appendChild(scrollDiv);
+    // Get the scrollbar width
+    var scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+    this.scrollBarWidth = scrollbarWidth;
+    // Delete the DIV 
+    document.body.removeChild(scrollDiv);
+  }
+
+  getValoracioCriteris(criteris){
+    var valor = 0;
+    criteris.forEach(criteri => {
+      if(criteri.checked){
+        valor += criteri.valor;
+      }
+    });
+    return valor != 0 ? parseFloat(valor.toFixed(2)) : 0;
+  }
+  getNotaExamen(preguntes){
+    var valor = 0;
+    preguntes.forEach(pregunta => {
+      valor += this.getValoracioCriteris(pregunta.criteris);
+    });
+    return valor != 0 ? parseFloat(valor.toFixed(2)) : 0;
+  }
+
 
 }
